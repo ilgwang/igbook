b203c1d3caa6128850e4e32fc9ed362b17c45294
ilgwang <85527016+ilgwang@users.noreply.github.com>
2022-10-15 19:48:19 +0900

    Add files via upload

---
+++ src/main/java/igbook/lib/LibApplication.java
@@ -0,0 +1,14 @@
+package igbook.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class LibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibApplication.class, args);
+	}
+	
+}
